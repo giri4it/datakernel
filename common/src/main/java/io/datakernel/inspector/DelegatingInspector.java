e3e2fc88562d6f2d074cff2f5beec4c3140bdd1a
Dmitry Volvach <dmitry@datakernel.io>
2019-01-14 16:38:56 +0200

    Provide nullability annotations for eventloop module

--- common/src/main/java/io/datakernel/inspector/DelegatingInspector.java
+++ common/src/main/java/io/datakernel/inspector/DelegatingInspector.java
@@ -1,11 +1,16 @@
 package io.datakernel.inspector;
 
+import java.util.Arrays;
 import java.util.List;
 
 public final class DelegatingInspector<I extends BaseInspector<I>> implements BaseInspector<I> {
 	protected final List<I> delegates;
 
 	public DelegatingInspector(List<I> delegates) {this.delegates = delegates;}
+
+	public static <I extends BaseInspector<I>> DelegatingInspector<I> of(I... delegates) {
+		return new DelegatingInspector<I>(Arrays.asList(delegates));
+	}
 
 	@Override
 	public <T extends I> T lookup(Class<T> type) {
